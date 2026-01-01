5fe2ad284072a83ee739d8020ab2584d90f52670
John Schmidt <john@john2143.com>
2026-01-01 16:46:53 -0500

    Temp

--- client/src/web.rs
+++ client/src/web.rs
@@ -2,14 +2,22 @@ use bevy::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::HtmlCanvasElement;
 
-use raw_window_handle::{HasRawWindowHandle, WebHandle};
+use raw_window_handle::HasRawWindowHandle;
 
 pub struct WebPlugin;
 
 impl Plugin for WebPlugin {
     fn build(&self, app: &mut App) {
-        let mut window_desc = WebHandle::empty();
-        handle.id = 1;
-        let canvas = HtmlCanvasElement::window().unwrap();
+        //let mut window_desc = WebHandle::empty();
+        //handle.id = 1;
+        let window = web_sys::window().unwrap();
+        let document = window.document().unwrap();
+        let canvas = document
+            .get_element_by_id("bevy-canvas")
+            .unwrap()
+            .dyn_into::<HtmlCanvasElement>()
+            .unwrap();
+
+        info!("We found the canvas element: {:?}", canvas);
     }
 }
