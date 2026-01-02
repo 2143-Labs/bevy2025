7018af2ede46de70ba0f769e08d84b00a900c2c2
John Schmidt <john@john2143.com>
2026-01-02 02:40:17 -0500

    temp

--- client/src/web.rs
+++ client/src/web.rs
@@ -22,5 +22,27 @@ impl Plugin for WebPlugin {
         inhtml.push_str("Hello from Bevy Web!");
 
         info!("We found the canvas element: {:?}", canvas);
+
+        let ws = web_sys::WebSocket::new("ws://71.126.177.34:25556/").unwrap();
+
+        let onmessage_callback = Closure::wrap(Box::new(move |e: web_sys::MessageEvent| {
+            if let Ok(txt) = e.data().dyn_into::<js_sys::JsString>() {
+                let msg = txt.as_string().unwrap();
+                info!("Received message: {}", msg);
+            } else {
+                info!("Received non-text message");
+            }
+        }) as Box);
+
+        ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+        onmessage_callback.forget();
+
+        ws.set_onopen(Some(
+            Closure::wrap(Box::new(move |_| {
+                info!("WebSocket connection opened");
+            }) as Box<dyn FnMut(_)>)
+            .as_ref()
+            .unchecked_ref(),
+        ));
     }
 }
